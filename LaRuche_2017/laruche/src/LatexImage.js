75c3ef57a2e06f957548d440768c2ee07198d2b0
Nathan Buskulic <nathan.buskulic@outlook.fr>
2017-05-30 16:53:09 +0200

    Ajout de l'entête, ajout de la gestion du Latex, Et transformation de l'onglet Enoncé

---
+++ LaRuche_2017/laruche/src/LatexImage.js
@@ -0,0 +1,29 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var Embed = Quill.import('blots/embed'); //On récupère la bonne class
+var LatexImage = (function (_super) {
+    __extends(LatexImage, _super);
+    function LatexImage() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    LatexImage.create = function (value) {
+        var node = _super.create.call(this, value); //On utilise la fonction create de Embed
+        node.innerHTML = "" + value; //On écrit notre valeur entre les span qui nous intéresses
+        return node;
+    };
+    return LatexImage;
+}(Embed));
+LatexImage.blotName = 'LatexImage'; //on définit le BlotName (celui à utiliser dans insertEmbered par exemple)
+LatexImage.className = 'surligne_Latex'; //On définit sa classe
+LatexImage.tagName = 'span'; //On définit les balises qui vont entourer cette class
+Quill.register({
+    'formats/latexImage': LatexImage
+});
---
+++ LaRuche_2017/laruche/src/LatexImage.ts
@@ -0,0 +1,16 @@
+var Embed = Quill.import('blots/embed'); //On récupère la bonne class
+
+class LatexImage extends Embed { //On crée notre class fille de Embed pour que nos variables soit intouchables
+  static create(value) {
+      let node = super.create(value);//On utilise la fonction create de Embed
+      node.innerHTML = `${value}`; //On écrit notre valeur entre les span qui nous intéresses
+      return node;
+    }
+}
+
+LatexImage.blotName = 'LatexImage'; //on définit le BlotName (celui à utiliser dans insertEmbered par exemple)
+LatexImage.className = 'surligne_Latex'; //On définit sa classe
+LatexImage.tagName = 'span';//On définit les balises qui vont entourer cette class
+Quill.register({ //On l'enregistre pour pouvoir la réutiliser
+    'formats/latexImage': LatexImage
+});
