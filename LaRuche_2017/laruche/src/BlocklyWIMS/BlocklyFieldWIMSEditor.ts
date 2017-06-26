10c37cba572fc847f88e1701ff50071e7c0b9d42
Damir Buskulic <damir.buskulic@ligo.org>
2017-06-26 22:25:24 +0200

    new html2canvas call

--- LaRuche_2017/laruche/src/BlocklyWIMS/BlocklyFieldWIMSEditor.ts
+++ LaRuche_2017/laruche/src/BlocklyWIMS/BlocklyFieldWIMSEditor.ts
@@ -17,8 +17,8 @@ goog.require('goog.userAgent');
 
 
 /**
- * Class for a Quill editor on a block. The Quill editor should have been
- * initialized and placed in a (possibly invisible) div element
+ * Class for a Quill editor on a block. The Quill editor is initialized
+ * and placed in a div element that is managed by the Blockly.ExternalDiv class
  * @param {string} content The text to be put in the editor.
  * @param {string=} opt_alt Optional alt text for when block is collapsed.
  * @extends {Blockly.Field}
@@ -103,14 +103,15 @@ Blockly.FieldWIMSEditor.prototype.computePlaceholderImage_ = function() {
   if (this.placeholderImageElement_ && this.editorDivId_) {
     var fieldTmp = {x:this};
     var editorDiv = document.getElementById(this.editorDivId_)
-    html2canvas(editorDiv, {
-        onrendered: function (canvas) {
+    html2canvas(editorDiv,{logging:true}).then(function(canvas) {
             var canvas_url = canvas.toDataURL();
             var width = fieldTmp.x.width_;
             var height = fieldTmp.x.height_;
             fieldTmp.x.placeholderImageElement_.setAttribute("href", canvas_url);
-        }
-    });
+          }, function(err){
+
+            console.log("html2canvas error:");}
+          );
   }
 }
 
